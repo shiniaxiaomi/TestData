efafeb85679829e160ba7da684017c14d2a8d94c
陆英杰 <34340797+shiniaxiaomi@users.noreply.github.com>
2019-09-27 00:24:42 +0800

    bc

--- src/Heap/MyHeap.java
+++ src/Heap/MyHeap.java
@@ -4,11 +4,9 @@ package Heap;
  * Created by Administrator on 2019/9/26.
  */
 public interface MyHeap {
-
-    public void add(int value) throws Exception;
-    public void build(int[] arr) throws Exception;
-    public int poll();
-    public int peek();
-    public boolean isEmpty();
-
+    public void add(int value) throws Exception;//添加元素
+    public void build(int[] arr) throws Exception;//通过数组构建最大堆
+    public int poll();//获取并删除最大值
+    public int peek();//获取但不删除最大值
+    public boolean isEmpty();//判空
 }
